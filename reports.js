6e1d9d44e94ea2c7569a713d515a0286f04cea79
teedoo <415577188@qq.com>
2014-03-28 11:14:52 -0400

    clean up arcticserver directory

--- reports.js
+++ reports.js
@@ -1,36 +1,53 @@
-var pointsData = {
-	"type": "FeatureCollection", "features":  [    
-	{
-        "type": "Feature",
-    	"properties": {
-    		"name": "United States",
-        },
-        "geometry": {
-    	   "type": "Point",
-    	   "coordinates": [-97, 38]
-	   }
-    },
+// var pointsData = {
+// 	"type": "FeatureCollection", "features":  [    
+// 	{
+//         "type": "Feature",
+//     	"properties": {
+//     		"name": "United States",
+//         },
+//         "geometry": {
+//     	   "type": "Point",
+//     	   "coordinates": [-97, 38]
+// 	   }
+//     },
 
-    {
-        "type": "Feature",
-        "properties": {
-            "name": "Russia",
-        },
-        "geometry": {
-            "type": "Point",
-            "coordinates": [100, 60]
-        }
-    },
+//     {
+//         "type": "Feature",
+//         "properties": {
+//             "name": "Russia",
+//         },
+//         "geometry": {
+//             "type": "Point",
+//             "coordinates": [100, 60]
+//         }
+//     },
 
-    {
-        "type": "Feature",
-        "properties": {
-            "name": "Greenland",
-        },
-        "geometry": {
-            "type": "Point",
-            "coordinates": [40, 72]
-        }
-    }
-    ]
-}
+//     {
+//         "type": "Feature",
+//         "properties": {
+//             "name": "Greenland",
+//         },
+//         "geometry": {
+//             "type": "Point",
+//             "coordinates": [40, 72]
+//         }
+//     }
+//     ]
+// }
+
+var Parse = require('parse').Parse;
+Parse.initialize("g9E0CvsnPFgymkq8FxTN0khh9FZ5sqbaqsoN6GfH", 
+    "R83Wi2r7ndSyA3gFUDqk6f3tZ9RP1Sn7WO3L9q3G");
+
+var Conference = Parse.Object.extend("Conference");
+//query
+var query = new Parse.Query(Conference);
+query.limit(1000);
+query.find({
+  success: function(results) {
+    res.send(results);
+  },
+  error: function(error) {
+    res.send("failed");
+  }
+});
