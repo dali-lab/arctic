d7733cd5beb464de1ce872d3730d7d09319057cf
krazemon <stephenmalina@gmail.com>
2014-04-02 14:17:20 -0400

    Change directory structure
    Move imgs into imgs folder
    Move leaflet into lib folder

--- point_connector.js
+++
@@ -1,9 +0,0 @@
-function addConnection(p1, p2) {
-		L.polyline([p.coordinates, p2.coordinates], {color: 'red'}).addTo(map);
-}
-
-function addConnections(data) {
-	for points in data {
-		addConnection(p);
-	}
-}
